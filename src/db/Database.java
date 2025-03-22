7b368a3b5590d959cf699fcfa222c1896ede2a74
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-03-22 05:32:58 -0700

    add database class

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,4 +1,38 @@
 package db;
 
+import db.exception.*;
+import java.util.ArrayList;
+
 public class Database {
+    private static ArrayList<Entity> entities = new ArrayList<>();
+    private static int identifier = 1;
+
+    private Database() {}
+
+    public static void add(Entity e) {
+        entities.add(e);
+        e.id = identifier;
+        identifier++;
+    }
+
+    public static Entity get(int id) throws EntityNotFoundException {
+        for (Entity e : entities) {
+            if (e.id == id) {
+                return e;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void delete(int id) throws EntityNotFoundException {
+        Entity e = get(id);
+        entities.remove(e);
+    }
+
+    public static void update(Entity e) throws EntityNotFoundException {
+        get(e.id);
+        int index = entities.indexOf(e);
+        entities.set(index, e);
+    }
+
 }
