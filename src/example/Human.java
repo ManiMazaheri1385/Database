46427a6ae1e913e096b97a9f0d25e5cd3bef99b5
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-03-22 06:51:48 -0700

    add human class

--- src/example/Human.java
+++ src/example/Human.java
@@ -1,4 +1,12 @@
 package example;
 
-public class Human {
+import db.*;
+
+public class Human extends Entity {
+    public String name;
+
+    public Human(String name) {
+        this.name = name;
+    }
+
 }
