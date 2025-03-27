5a71cbd20b34912484ebbdb40f0f1239f001497d
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-03-27 19:19:46 +0330

    initialize date fields in database class methods

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,6 +1,7 @@
 package db;
 
 import db.exception.*;
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -15,6 +16,13 @@ public class Database {
     public static void add(Entity entity) throws InvalidEntityException {
         Validator validator = validators.get(entity.getEntityCode());
         validator.validate(entity);
+
+        if (entity instanceof Trackable) {
+            Date now = new Date();
+            ((Trackable) entity).setCreationDate(now);
+            ((Trackable) entity).setLastModificationDate(now);
+        }
+
         entity.id = identifier;
         entities.add(entity.clone());
         identifier++;
@@ -37,7 +45,14 @@ public class Database {
     public static void update(Entity entity) throws EntityNotFoundException, InvalidEntityException {
         Validator validator = validators.get(entity.getEntityCode());
         validator.validate(entity);
+
         get(entity.id);
+
+        if (entity instanceof Trackable) {
+            Date now = new Date();
+            ((Trackable) entity).setLastModificationDate(now);
+        }
+
         int index = entities.indexOf(entity);
         entities.set(index, entity.clone());
     }
