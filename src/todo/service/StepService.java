a072dc08faf7237787424490eefb5e4be2d62f65
ManiMazaheri1385 <manimazaheri.13852006@gmail.com>
2025-03-31 01:27:02 +0330

    update add step method in step service class

--- src/todo/service/StepService.java
+++ src/todo/service/StepService.java
@@ -7,12 +7,29 @@ import todo.entity.Step;
 public class StepService {
     private StepService() {}
 
-    public static void saveStep(int taskRef, String title) {
-        Step step = new Step(taskRef, title);
+    public static void addStep() {
+        System.out.print("TaskID: ");
+        int taskID = Integer.parseInt(scanner.nextLine().trim());
+
+        System.out.print("Title: ");
+        String title = scanner.nextLine().trim();
+
+        Step step = new Step(taskID, title);
+
         try {
             Database.add(step);
         }
         catch (InvalidEntityException e) {
+            TaskService.setAsInProgress(taskID);
+            System.out.println("Step saved successfully.");
+            System.out.println("ID: " + step.id);
+            System.out.println("Creation Date: " + step.creationDate);
+        } catch (InvalidEntityException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
             System.out.println(e.getMessage());
         }
         System.out.println("Step saved successfully.\n" + "ID: " + step.id + "\nCreation Date: " + step.creationDate);
